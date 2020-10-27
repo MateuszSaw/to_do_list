38fa01c1b8ca733c47067cd4983ddd530d7a3894
Mateusz Sawicki <mateuszsawicki1509@gmail.com>
2020-10-27 16:54:16 +0100

    adding new tasks

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 {
-  const tasks = [
-    {
+  const tasks = [{
     content: "odrobić zadanie z YouCode",
     done: false,
-    }
-  ];
+  }];
 
   const render = () => {
     let htmlString = "";
 
     for (const task of tasks) {
       htmlString += `
-      <li>
-        ${task.content}
+      <li class="listItem">
+        <button class="listItem__check">${task.done ? "✔" :""}</button>
+        <p class="listItem__paragraph">${task.content}</p>
+        <button class="listItem__delete">🗑</button>
       </li>
       `;
     }
@@ -22,8 +22,31 @@
 
   };
 
+  const addNewTask = (newTaskContent) => {
+    tasks.push({
+      content: newTaskContent,
+    });
+
+    render();
+  };
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+    if (newTaskContent === "") {
+      return;
+    }
+
+    addNewTask(newTaskContent);
+  };
+
   const init = () => {
     render();
+
+    const form = document.querySelector(".js-form");
+    form.addEventListener("submit", onFormSubmit);
   };
 
   init();
